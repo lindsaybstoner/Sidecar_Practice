e120c11fe452f60d2998f29f411c6e6b3dd2f398
Lindsay Stoner <lindsaybstoner@gmail.com>
2019-08-28 22:12:03 -0400

    adding multiple things to state

--- practice/src/App.js
+++ practice/src/App.js
@@ -3,13 +3,24 @@ import React, { Component } from 'react';
 class App extends Component {
   state = {
     clicked: false,
+    textInfo: "click the button",
   }
+
+  handleChange = () => {
+    this.setState({
+      clicked: true,
+      textInfo: "you've clicked the button"
+    });
+  }
+
   render() {
     return (
       <>
         <div>
           <header>
-            <p>Hello I am a boring react app trying to learn redux!</p>
+            <p>Hello I am a boring react app trying to learn redux! Hi</p>
+            <button onClick={this.handleChange}>Click ME!!</button>
+            <p>{this.state.textInfo}</p>
           </header>
         </div>
       </>
