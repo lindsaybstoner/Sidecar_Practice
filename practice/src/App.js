ab22f34781e2a439a64efccf872611bf8640e906
Lindsay Stoner <lindsaybstoner@gmail.com>
2019-09-01 13:44:03 -0400

    adding in hooks instead of redux

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,31 +1,32 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+// useState and useEffect are built in functionality to mimic lifecylcle methods
 
-class App extends Component {
-  state = {
-    clicked: false,
-    textInfo: "click the button",
-  }
+const Example = () => {
+  // cant be in a class --- must be in a function whether that is an arrow or traditional
+  // Declare a new state variable
+  // by connecting the variable set perviously in state to the action to take
+  // in order to change it allows it for specifificity of action to variable pairing
 
-  handleChange = () => {
-    this.setState({
-      clicked: true,
-      textInfo: "you've clicked the button"
-    });
-  }
+  const [count, setCount] = useState(0); //set equal to the first time is the default setting
+  const [age, setAge] = useState(42);
 
-  render() {
-    return (
-      <>
-        <div>
-          <header>
-            <p>Hello I am a boring react app trying to learn redux! Hi</p>
-            <button onClick={this.handleChange}>Click ME!!</button>
-            <p>{this.state.textInfo}</p>
-          </header>
-        </div>
-      </>
-    )
-  }
-};
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  }, [count]);
+  // useEffect is the combination of componentDidMount, componentDidUpdate, componentWillUnmount
+  // second argument of [count] Only re-run the effect if count changes
 
-export default App;
+  return (
+    <div>
+      <p>This is your age {age}</p>
+      <button onClick={() => setAge(age + 1)}> Increase your age </button>
+      <button onClick={() => setAge(age - 1)}> Decrease your age </button>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+export default Example;
